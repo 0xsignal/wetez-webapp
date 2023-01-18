4d6ec02533a347338e5c43d4adf111ef70adf176
曹操 <xuzhao.sgn@gmail.com>
2023-01-18 16:54:20 +0800

    resolve some page style

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/ipfs':{page:'/ipfs'},
+      '/dashboard':{page:'/dashboard'},
+      '/settings':{page:'/settings'},
+      '/signup':{page:'/signup'},
+      '/login':{page:'/login'},
+      '/premium':{page:'/premium'},
+      '/test':{page:'/test'},
+    }
+  }
 }
 
 module.exports = nextConfig
